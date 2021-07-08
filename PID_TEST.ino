3745eaea4cb0ae6da74c83eda25ef7bda2b3aa7b
pyae soan aung <server01.psa1981@gmail.com>
2021-07-08 07:38:14 +0630

    rpm counter

--- PID_TEST.ino
+++ PID_TEST.ino
@@ -13,10 +13,13 @@
 
 unsigned long prev_MilliSec = 0;   
 
-int pwm = -255/2;
+int pwm = 0;
 
 volatile long encoder_counts = 0;
 long prev_encoder_counts = 0;
+
+double set_rpm = 0;
+double actual_rpm = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -27,7 +30,9 @@ void loop() {
   unsigned long now_t = millis();
   if (now_t - prev_MilliSec >= 100)   {   
    
-       
+    long delta_t = now_t - prev_MilliSec;
+    getMotorSpeed(delta_t);
+    
     if ( pwm > 0 ) 
     {
       Forward();
@@ -43,9 +48,9 @@ void loop() {
     prev_MilliSec = now_t;
 
     //Serial.print(" ");Serial.print(set_rpm);
-    //Serial.print(" ");Serial.print(actual_rpm);
+    Serial.print(" ");Serial.print(actual_rpm);
     //Serial.print(" ");Serial.print(pwm);
-    Serial.print(" ");Serial.print(encoder_counts);
+    //Serial.print(" ");Serial.print(encoder_counts);
     Serial.println();
     }
 }
--- motor.ino
+++ motor.ino
@@ -1,7 +1,14 @@
 void setupMotors() { 
+  actual_rpm = 0;
+  pwm = 255/2;
   Release();
 }
 
+void getMotorSpeed(unsigned long time_)
+{
+  actual_rpm = double( (encoder_counts - prev_encoder_counts) * 60000) / double ( time_ * new_enc_ticks );
+  prev_encoder_counts = encoder_counts;
+}
 void Forward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
